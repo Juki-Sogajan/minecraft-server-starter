893cbb85fde0afdfc0593b1fb87bb4b25878e32f
スジャん <111575677+Juki-Sogajan@users.noreply.github.com>
2024-05-22 14:49:02 +0900

    minecraft.sh を追加

---
+++ minecraft.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+# Config
+SERVER_JAR_FILE=
+SERVER_DIR="/opt/minecraft"
+
+PORT=25565
+
+OTHER_OPTION=
+
+
+# Script
+cd $SERVER_DIR
+java -jar $SERVER_JAR_FILE --nogui --port $PORT $OTHER_OPTION
